8653d8c4e2f85f6dc703825817639b55982b15ae
Jacob <Jtwalsh7@gmail.com>
2020-05-25 21:11:14 -0400

    began refactroing mastermind, split play and check functions in to seperate components

--- mastermind.rb
+++ mastermind.rb
@@ -21,7 +21,17 @@ module Pins
 		end
 		puts ""
 		color = COLORS[gets.chomp.to_i-1]
+		valid_choice?(color)
 		PEGS[color]
+	end
+
+	def valid_choice?(color)
+		if COLORS.slice(0,6).any?(color)
+			return
+		else
+			puts "invalid choice"
+			pick_peg
+		end
 	end
 end
 
@@ -39,12 +49,13 @@ class Board
 			guesses[i] =[" ", " ", " ", " "]
 			check[i] = [" ", " ", " ", " "]
 		end
+		@num_turns = 8
 	end
 
 	def display
 		p "    MasterMind " 
 		p "--------------------"
-		for i in 0..9
+		for i in 0...@num_turns
 			puts "| #{guesses[i][0]} | #{guesses[i][1]} | #{guesses[i][2]} | #{guesses[i][3]} ||#{check[i][0]}|#{check[i][1]}|"
 			puts "|___|___|___|___||#{check[i][2]}|#{check[i][3]}|"
 			puts "======================"
@@ -56,21 +67,27 @@ class Board
 	end
 
 	def reveal(round,black_pins, white_pins)
-
 		for i in 0...black_pins
 			check[round][i] =  PEGS[:black]
 		end
 
-		for i in black_pins..black_pins+white_pins-1
+		for i in black_pins...black_pins+white_pins
 			check[round][i]=PEGS[:white]
 		end
+	end
 
+	def player_wins?(guess, correct_pattern)
+		return guess == correct_pattern
+	end
+
+	def full?
+		return guesses[@num_turns-1][3]!=" "
 	end
 end
 
 class Coder
-	
 	include Pins
+
 	def random_pattern
 		@pattern =[]
 		for i in 0..3
@@ -105,14 +122,11 @@ class Game
 		@board = Board.new
 		@mastermind = Coder.new
 		@board.display
+		@round_guess =[]
 	end
 
-	def player_wins?
-		return @right_guess ==4
-	end
-
-	def full_board?
-		return @board.guesses[9][3]!=" "
+	def player_wins?(correct_pattern)
+		return @board.player_wins?(@round_guess, correct_pattern)
 	end
 
 	def copy_pattern(pattern)
@@ -123,70 +137,73 @@ class Game
 		return copy
 	end
 
-	def check(correct_pattern,guess)
-		@right_guess = 0
-		right_colors =0
-		coppied_pattern = copy_pattern(correct_pattern)
+	def pattern_this_round(round)
+		suffixes =["st", "nd", "rd", "th"]
+		for i in 0..3
+			puts "Round #{round}"
+			puts "pick your #{i+1}#{suffixes[i]} peg"
+			@round_guess[i] = pick_peg
+			@board.set(round-1,i,@round_guess[i])
+			@board.display
+		end
+	end
 
-		coppied_pattern.each_with_index do |value, index|
-			if value == guess[index]
+	def right_position?(correct_pattern,player_guess)
+		correct_pattern.each_with_index do |correct_peg, index|
+			if player_guess[index] == correct_peg
 				@right_guess += 1
-				guess[index] = nil
-				coppied_pattern[index] = nil
+				player_guess[index] = nil
+				correct_pattern[index] = nil
 			end
 		end
+	end
 
-		guess.delete(nil)
-		coppied_pattern.delete(nil)
-
-		coppied_pattern.each do |value|
-			if guess.any?(value)
+	def correct_colors(correct_pattern,player_guess)
+		right_colors = 0
+		correct_pattern.each do |value|
+			if player_guess.any?(value)
 				right_colors +=1
 			end
 		end
-
-		return [@right_guess,right_colors]
+		return right_colors
 	end
 
-	def play()
-		puts "would you like to play against the computer?"
-		computer_play = gets.chomp.downcase[0]=='y'
+	def check(correct_pattern)
+		@right_guess = 0
 
+		correct_pattern = copy_pattern(correct_pattern)
+		player_guess = copy_pattern(@round_guess)
+
+		right_position?(correct_pattern,player_guess)
+
+		player_guess.delete(nil)
+		correct_pattern.delete(nil)
+
+		white_pins = correct_colors(correct_pattern,player_guess)
+
+		return [@right_guess,white_pins]
+	end
+
+	def choose_opponent()
+		puts "would you like to play against the computer? [yes/no]"
+		computer_play = gets.chomp.downcase[0]=='y'
 		if computer_play
 			correct_pattern = @mastermind.random_pattern
 		else
 			correct_pattern = @mastermind.choose_pattern
 		end
+	end
 
-		puts ""
-		round =1
-		@right_guess = 0
-		until player_wins? or full_board?
-			suffixes =["st", "nd", "rd", "th"]
-			guess =[]
-
-			for i in 0..3
-				puts "Round #{round}"
-				puts "pick your #{i+1}#{suffixes[i]} peg"
-				guess[i] = pick_peg
-				@board.set(round-1,i,guess[i])
-				@board.display
-			end
-
-			black_pins, white_pins = check(correct_pattern,guess)
-			@board.reveal(round-1,black_pins, white_pins)
-			@board.display
-			round +=1
-		end
-
+	def end_prompt(correct_pattern)
 		puts "the correct correct_pattern is"
 		@mastermind.show_pattern
 
-		if player_wins?
+		if player_wins?(correct_pattern)
 			puts "You Win! Congratz."
 		else
 			puts "You Lost. Unfortunate"
 		end
+
 		puts" Play again? [yes/no]"
 		response = gets.chomp[0].downcase
 
@@ -196,9 +213,25 @@ class Game
 		else
 			puts "See ya"
 		end
+	end
 
+	def play()
+		correct_pattern = choose_opponent
+		puts ""
+		round =1
+
+		@right_guess = 0
+
+		until player_wins?(correct_pattern) or @board.full?
+			pattern_this_round(round)
+			black_pins, white_pins = check(correct_pattern)
+			@board.reveal(round-1,black_pins, white_pins)
+			@board.display
+			round +=1
+		end
+
+		end_prompt(correct_pattern)
 	end
 end
 
-my_game = Game.new
-my_game.play
+Game.new.play
